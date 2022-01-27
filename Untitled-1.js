d8c431c821fce9a830a97f869e4be8fd7ab86a34
Byanca <79319402+byancamari@users.noreply.github.com>
2022-01-27 20:43:20 -0300

    Arquivo em js

---
+++ Untitled-1.js
@@ -0,0 +1,21 @@
+<><meta charset="UTF-8"></meta><script>
+
+    function mostra(frase) {document.write(frase)};
+    {"}"}
+
+    function pulaLinha() {document.write("<br><br>")};
+    {"}"}
+
+    var titulo = "Bem-vindo ao meu programa!";
+
+    mostra("<h1>" + titulo + "</h1>");
+    pulaLinha();
+
+    mostra("Até que não é tão ruim Jorge!");
+    pulaLinha();
+    mostra("Contudo, é muito confuso ...");
+    pulaLinha();
+
+    document.write("Melhorando aos poucos!");
+
+</script></>
